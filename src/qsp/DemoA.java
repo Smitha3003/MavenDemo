72c89283b7bd9619b0c317cfff90f558b84063d8
hulisandra <hulisandra@gmail.com>
2018-03-12 17:17:01 +0530

    first commit

---
+++ src/qsp/DemoA.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("hiii",true);
+	}
+
+}
+//Jre will not run the maven project
